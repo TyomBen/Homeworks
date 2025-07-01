bdda10566a9f344e3530c5383152511ce6f417a6
TyomBen <artyombenikyan@mail.ru>
2025-07-01 16:04:17 +0400

    Homeworks

---
+++ 08.06.2025/Lesson2/Password_hide/password_hide.js
@@ -0,0 +1,6 @@
+document.getElementById('eyeIcon').addEventListener('click', function() {
+    document.getElementById('password').type == 'password' 
+    ? document.getElementById('password').type = 'text' 
+    : document.getElementById('password').type = 'password';
+    
+});
