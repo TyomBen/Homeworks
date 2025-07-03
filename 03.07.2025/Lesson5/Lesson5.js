a01899e9a47eaf30557dd47a0183150356ce6442
TyomBen <artyombenikyan@mail.ru>
2025-07-03 18:09:04 +0400

    Lesson 5

--- 03.07.2025/Lesson5/Lesson5.js
+++ 03.07.2025/Lesson5/Lesson5.js
@@ -45,3 +45,63 @@ function createAccount (accountHolder, accountNumber, balance = 0) {
 }
 //You need to write object inside of argument of the function like account2.addAccount({accountHolder : 'Some', accountNumber : '1234', balance : 20050})
 
+class CreateAccount {
+    #balance = 0;
+    #accountHolder = null;
+    #accountNumber = null;
+    constructor (balance, accountHolder, accountNumber){
+        this.#balance = balance;
+        this.#accountHolder = accountHolder;
+        this.#accountNumber = accountNumber;
+    }
+    deposit (deposit) {
+        if(deposit > 0) {
+            return this.#balance += deposit;
+        }
+        return 'There is something else, please try later';
+    }
+    withdraw (amount) {
+        if(amount > 0 && amount < this.#balance) {
+            return this.#balance -= amount;
+        }
+        return 'There is something else, please try later'
+    }
+    getBalance () {
+        return this.#balance;
+    }
+    getAllData () {
+        return {
+            balance : this.#balance,
+            accountHolder : this.#accountHolder,
+            accountNumber : this.#accountNumber
+        }
+    }
+}
+
+//You need to write object inside of argument of the function like account2.addAccount({accountHolder : 'Some', accountNumber : '1234', balance : 20050})
+
+class Bank {
+    #accounts = [];
+
+    addAccount(account) {
+        this.#accounts.push(account);
+        return this.#accounts;
+    }
+    findAccount(acc_num) {
+        const res = [];
+        this.#accounts.forEach(({accountNumber, accountHolder}) => {
+            if(acc_num.includes(accountNumber)) {
+                res.push(accountNumber)
+            }
+        })
+        return res;
+    }
+    getTotalBalance() {
+        const res = this.#accounts.reduce((acc, {balance}) => {
+            return acc + balance;
+        }, 0)
+        return res;
+    }
+}
+
+//You need to write object inside of argument of the function like account2.addAccount({accountHolder : 'Some', accountNumber : '1234', balance : 20050})
